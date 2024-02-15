ba047b73a371b651e5df0b905350ece9d55797d2
Nadia-kaf <nadiabokordedzi@gmail.com>
2024-02-15 15:50:23 +0000

    new change

--- src/App.js
+++ src/App.js
@@ -9,11 +9,7 @@ function App() {
         <p>
           Edit <code>src/Ap.js</code> and save to reload.
         </p>
-<<<<<<< HEAD
-        <p>This is Nadia</p>
-=======
         <p>Hello there</p>
->>>>>>> master
         <a
           className="App-link"
           href="https://reactjs.org"
